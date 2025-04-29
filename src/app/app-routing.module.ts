9d410e14211ac113a2eac3cc2a2cb38c552eab15
Jax <12698100+Jakatakz@users.noreply.github.com>
2025-04-29 02:30:39 -0400

    modiified app routing, comoponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
+import { RouterModule, Routes } from '@angular/router'; // so the application can have routing capability.
+import { HeroesComponent } from './heroes/heroes.component'; // gives router somewhere to go once you configure the routes.
 
 const routes: Routes = [
-  {path: 'heroes', component: HeroesComponent}
+  {path: 'heroes', component: HeroesComponent} // path: a string that matches the url in the browser address bar. Component: The component that the router should create when navigating to this route..
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], // method is called forRoot() because you configure the router at the applications root level. The forRoot() method supplies the service providers and directives needed for routing, and performs the initial navigation based on the current browser URL.
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
